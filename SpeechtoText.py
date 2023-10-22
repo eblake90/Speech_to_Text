4e05c889d97c567fc0172b864cf2bf5d270eeb07
Edward Blake <128470247+eblake90@users.noreply.github.com>
2023-10-22 13:57:21 +0200

    Update SpeechtoText.py

--- SpeechtoText.py
+++ SpeechtoText.py
@@ -2,28 +2,28 @@ import os
 import openai
 from pydub import AudioSegment
 
-# Set the paths for ffmpeg and ffprobe
+# Setting the paths for ffmpeg and ffprobe
 AudioSegment.converter = r"C:\\Users\\Owner\\OneDrive\Documents\\ffmpeg-2023-10-18-git-e7a6bba51a-full_build\\bin\\ffmpeg.exe"
 AudioSegment.ffprobe = r"C:\\Users\\Owner\\OneDrive\Documents\\ffmpeg-2023-10-18-git-e7a6bba51a-full_build\\bin\\ffprobe.exe"
 
-# Set OpenAI API key
-openai_api_key = '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'
+# Setting OpenAI API key
+openai_api_key = 'Use your own API, thank you'
 openai.api_key = openai_api_key
 
 # Source and destination directories
 source_directory = r'C:\\Users\\Owner\\OneDrive\\Documents\\2023-2024\\GF AI\\Recordings'
 destination_directory = r'C:\\Users\\Owner\\OneDrive\\Documents\\2023-2024\\GF AI\\Text'
 
-# Iterate over the MP3 files in the source directory
+# Iterating over the MP3 files in the source directory
 for filename in os.listdir(source_directory):
     if filename.endswith(".mp3"):
         mp3_path = os.path.join(source_directory, filename)
 
-        # Transcribe the audio using Whisper API
+        # Transcribing the audio using Whisper API
         with open(mp3_path, 'rb') as audio_file:
             transcript = openai.Audio.transcribe("whisper-1", audio_file)
 
-        # Write the transcription to the destination directory
+        # Writing the transcription to the destination directory
         text_path = os.path.join(destination_directory, filename.replace('.mp3', '.txt'))
         with open(text_path, 'w') as text_file:
             text_file.write(transcript['text'])
